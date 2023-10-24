ade0afe01565347c307aac72afabc6112b97726a
penguinsrpog <mtychung09@gmail.com>
2023-10-24 17:58:05 -0400

    rip i failed the frq just like my tests

---
+++ Distance.java
@@ -0,0 +1,35 @@
+
+
+
+import java.util.*;
+public class Distance<kilometers>{
+    private double km;
+    public void distance1(kilometers km){
+        toLeagues = distance1.km() / 5.556
+    }
+    public double getToLeagues{
+        return toLeagues
+    }
+    public void distance2(kilometers km){
+        toMiles = distance1.km() / 1.609;
+    }
+    public double getToMiles(){
+        return toMiles;
+    }
+    public double doublesMiles(){
+        doublesMiles = toMiles * 2;
+    }
+    public double getDoublesMiles(){
+        return getDoublesMiles;
+    }
+    public double getKilometers{
+        getKilometers = distance1.km();
+    }
+    public double getGetKilometers{
+        return getKilometers;
+    }
+    
+}
+
+        //this is what i would use if i had time and ik its wrong rip
+        
--- main.java
+++ main.java
@@ -1,17 +1,44 @@
-import java.util.*;
-class main{
-
-public static void main(String[] args){
-    System.out.println("amogus");
-    Scanner scanner = new Scanner(System.in);
-
-    System.out.println("input 1");
-    int input1 = scanner.nextInt();
-    System.out.println("name?");
-    String input2 = scanner.nextLine();
-    System.out.println("Your name is" + input2);
 
 
-}
+    import java.util.*;
+    public class main{
+        private double kilometers;
+        public kilometersSaid(double km){
+            kilometers = km;
+        }
+        public double getKilometers{
+            return kilometers
+        }
+        public static void Distance(String[] args){
+        System.out.println("What do you want to do?");
+        public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("what is your kilometers?");
+        double input = scanner.nextInt();
+        Scanner operation = new Scanner(System.in);
+        System.out.println("what would you like to convert to?");
+        String input1 = scanner.nextLine();
+        Object input2;
+        if (input1 == Kilometers){
+            input2 = input2 * 1.609;
+            System.out.println(input2);
 
-}
+
+        }
+        else if (input1 == Leagues){
+            input2 = input2 / 5.556;
+            System.out.println(input2);
+        }
+        else if (input1 == miles){
+            input2 = input2 / 1.609;
+        }
+
+        else if (input1 == doubleMiles){
+            input2 = input2 / 1.609
+            input2 = input2 * 2 
+
+        }
+        else{
+            return kilometersSaid
+        }
+    }
