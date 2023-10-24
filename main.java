6ffd08c279a38197500245c3fe7882349a047b1e
penguinsrpog <mtychung09@gmail.com>
2023-10-24 11:34:22 -0400

    new

---
+++ main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class main{
+
+public static void main(String[] args){
+    System.out.println("amogus");
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("input 1");
+    int input1 = scanner.nextInt();
+    System.out.println("name?");
+    String input2 = scanner.nextLine();
+    System.out.println("Your name is" + input2);
+
+
+}
+
+}
